0e3a809205f43bf7545b8a1f8e086e9062eea2db
carlostuchan <142187504+carlostuchan@users.noreply.github.com>
2023-09-25 21:47:23 -0600

    Add files via upload

---
+++ SEMANA 8/L7CJTH1190823/L7CJTH1190823/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L7CJTH1190823
+{
+    using System;
+
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Ingrese un número entero mayor que 0:");
+            int N;
+
+            while (true)
+            {
+                if (int.TryParse(Console.ReadLine(), out N) && N > 0)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Entrada no válida. Ingrese un número entero mayor que 0:");
+                }
+            }
+
+            int a = 0, b = 1;
+            int count = 0; 
+
+            
+            Console.WriteLine("Sucesión de Fibonacci hasta el término " + N + ":");
+            while (count < N)
+            {
+                Console.Write(a + " ");
+
+                int temp = a;
+                a = b;
+                b = temp + b;
+
+                count++;
+            }
+
+            Console.ReadLine(); 
+        }
+    }
+}
